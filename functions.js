79dc9d46fdb1a11177ca609081b2f3a423ad0620
alexopie <alex.oplawski@gmail.com>
2016-03-02 19:50:13 +0000

    pushing

--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@ function main () {
     init();
 }
 
-var A0;
+var A0; //Arrays for empty rectangle location
 var A1;
 var A2;
 var A3;
@@ -14,6 +14,11 @@ var fontdef;  // for text labeling
 var fontval;
 
 var tbarsize; // target reading bar size
+
+var A0_TB, A1_TB, A2_TB, A3_TB; // target bar locations based on user input
+var A0_Val, A1_Val, A2_Val, A3_Val; //sensor values
+
+
 
 function init(){
     var canvas = document.getElementById('myCanvas');
@@ -33,11 +38,41 @@ function init(){
     A1 = [width/2 + rectwidth/2, height/2];
     A2 = [width/2 - rectwidth/2, height/2 + rectwidth ];
     A3 = [width/2 - rectheight - rectwidth/2, height/2];
+
+    //Sensor Reading Values    
+    A0_Val = 50;
+    A1_Val = 50;
+    A2_Val = 50;
+    A3_Val = 50;
     
+    // Default values - User inputs values by clicking New Patient button
+    A0_TB = 50;
+    A1_TB = 50;
+    A2_TB = 50;
+    A3_TB = 50;
+    
+     //Button
+    var NewPatient = document.getElementById('NewPatient');
+    NewPatient.onclick = function()
+    {
+        var A0_string = window.prompt("Please enter the A0 Target Value for this patient:");
+        A0_TB = parseInt(A0_string);
+        
+        var A1_string = window.prompt("Please enter the A1 Target Value for this patient:");
+        A1_TB = parseInt(A1_string);
+        
+        var A2_string = window.prompt("Please enter the A2 Target Value for this patient:");
+        A2_TB = parseInt(A2_string);
+        
+        var A3_string = window.prompt("Please enter the A3 Target Value for this patient:");
+        A3_TB = parseInt(A3_string);
+    };
+
 
     window.requestAnimationFrame(draw);
 }
 
+ 
 
 
 function draw() {
@@ -58,14 +93,14 @@ function draw() {
     ctx.fillRect(A0[0], A0[1],rectwidth,rectheight);
     ctx.fillStyle = "#0000FF"; 
     ctx.font= fontdef;
-    ctx.fillText("A0",A0[0] + rectwidth/4 , A0[1] + fontval + 2);
+    ctx.fillText("A0", A0[0] + rectwidth/4 , A0[1] - fontval/2);
       
     //Rectangle A1    ***switch rectheight & rectwidth***
     ctx.fillStyle = "#FFFFFF";
     ctx.fillRect(A1[0], A1[1], rectheight, rectwidth);
     ctx.fillStyle = "#0000FF"; 
     ctx.font= fontdef;
-    ctx.fillText("A1",A1[0] + rectheight - fontval - 2 , A1[1] + rectwidth/2 + fontval/2 - 1);
+    ctx.fillText("A1", A1[0] + rectheight + fontval/2 , A1[1] + rectwidth/2 + fontval/2 - 1);
     
       
     //Rectangle A2
@@ -73,61 +108,99 @@ function draw() {
     ctx.fillRect(A2[0], A2[1],rectwidth,rectheight);
     ctx.fillStyle = "#0000FF"; 
     ctx.font= fontdef;
-    ctx.fillText("A2", A2[0] + rectwidth/4 , A2[1] + rectheight - 2 );
+    ctx.fillText("A2", A2[0] + rectwidth/4 , A2[1] + rectheight + fontval + 2 );
       
     //Rectangle A3    ***switch rectheight & rectwidth***
     ctx.fillStyle = "#FFFFFF";
-   ctx.fillRect(A3[0], A3[1],rectheight,rectwidth);
+    ctx.fillRect(A3[0], A3[1],rectheight,rectwidth);
     ctx.fillStyle = "#0000FF"; 
     ctx.font= fontdef;
-    ctx.fillText("A3",A3[0] + 2, A3[1] + rectwidth/2 + fontval/2 - 1);
+    ctx.fillText("A3", A3[0] - (fontval*1.5) , A3[1] + rectwidth/2 + fontval/2 - 1);
+    
+ 
+    //Implement once filler coder is written
     
     
+    //Rectangle A0_Fill
+    if((5>= (A0_Val - A0_TB)) && (-5<= (A0_Val - A0_TB)))
+    {
+        ctx.fillStyle = "#00FF00";
+        ctx.fillRect(A0[0], A0[1] + rectheight, rectwidth, -(A0_Val));
+    }
+    else
+    {
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(A0[0], A0[1] + rectheight, rectwidth, -(A0_Val));
+    }
+      
+    //Rectangle A1_Fill    ***switch rectheight & rectwidth***
+    if((5>= (A1_Val - A1_TB)) && (-5<= (A1_Val - A1_TB)))
+    {
+        ctx.fillStyle = "#00FF00";
+        ctx.fillRect(A1[0], A1[1], (A1_Val), rectwidth);
+    }
+    else
+    {
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(A1[0], A1[1], (A1_Val), rectwidth);
+    }
     
+      
+    //Rectangle A2_Fill
+    if((5>= (A2_Val - A2_TB)) && (-5<= (A2_Val - A2_TB)))
+    {
+        ctx.fillStyle = "#00FF00";
+        ctx.fillRect(A2[0], A2[1], rectwidth, (A2_Val));
+    }
+    else
+    {
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(A2[0], A2[1], rectwidth, (A2_Val));
+    }
+
+    //Rectangle A3_Fill    ***switch rectheight & rectwidth***
+    if((5>= (A3_Val - A3_TB)) && (-5<= (A3_Val - A3_TB)))
+    {
+        ctx.fillStyle = "#00FF00";
+        ctx.fillRect(A3[0] + rectheight, A3[1], -(A3_Val), rectwidth);
+    }
+    else
+    {
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(A3[0] + rectheight, A3[1], -(A3_Val), rectwidth);
+    }
+
+   
     //Target Range Rectangles - Blue
     
     //A0 target bar
     ctx.fillStyle = "#0000FF";
-    ctx.fillRect(A0[0], A0[1] + rectheight/2, rectwidth, tbarsize);
+    ctx.fillRect(A0[0], (A0[1] + rectheight) - A0_TB, rectwidth, tbarsize);
 
     //A1 target bar
     ctx.fillStyle = "#0000FF";
-    ctx.fillRect(A1[0] + rectheight/2, A1[1], tbarsize, rectwidth);
+    ctx.fillRect(A1[0] + A1_TB, A1[1], tbarsize, rectwidth);
 
     //A2 target bar
     ctx.fillStyle = "#0000FF";
-    ctx.fillRect(A2[0], A2[1] + rectheight/2, rectwidth, tbarsize);
+    ctx.fillRect(A2[0], A2[1] + A2_TB, rectwidth, tbarsize);
 
     //A3 target bar
     ctx.fillStyle = "#0000FF";
-    ctx.fillRect(A3[0] + rectheight/2, A3[1], tbarsize, rectwidth);
+    ctx.fillRect((A3[0] + rectheight) - A3_TB, A3[1], tbarsize, rectwidth);
+    
+
+    
+
     
     
-    
-    //Implement once filler coder is written
-    
-    /*
-    
-    //Rectangle A0_Fill
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(A0[0], A0[1],rectwidth,rectheight);
-
-      
-    //Rectangle A1_Fill    ***switch rectheight & rectwidth***
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(A1[0], A1[1], rectheight, rectwidth);
-
-      
-    //Rectangle A2_Fill
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(A2[0], A2[1],rectwidth,rectheight);
-
-
-    //Rectangle A3_Fill    ***switch rectheight & rectwidth***
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(A3[0], A3[1],rectheight,rectwidth);
-
-*/ 
-
     window.requestAnimationFrame(draw);
 }
+
+
+// <meta http-equiv="refresh" content="5" />
+// ^^^ ADD THIS LINE TO <HEAD> OF INDEX FOR AUTOREFRESH
+// CONTENT = # IS NUMBER OF SECONDS UNTIL REFRESH
+
+//Seems unnecessary as button still works...from body of index
+//    <a href="#" class="Button" id="NewPatient">New Patient</a>
